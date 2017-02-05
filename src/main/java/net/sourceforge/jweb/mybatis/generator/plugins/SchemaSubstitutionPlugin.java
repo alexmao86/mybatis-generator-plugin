30035134e35a77452d77d6e75a1da87d2a020aa4
alexmao86 <maoanapex88@163.com>
2017-02-05 20:57:32 +0800

    override initialized

--- src/main/java/net/sourceforge/jweb/mybatis/generator/plugins/SchemaSubstitutionPlugin.java
+++ src/main/java/net/sourceforge/jweb/mybatis/generator/plugins/SchemaSubstitutionPlugin.java
@@ -1,8 +1,6 @@
 package net.sourceforge.jweb.mybatis.generator.plugins;
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import java.lang.reflect.Field;
 import java.util.List;
 
 import org.mybatis.generator.api.FullyQualifiedTable;
@@ -10,41 +8,45 @@ import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 
 public class SchemaSubstitutionPlugin  extends PluginAdapter {
+	private static final String RECUR_MARK = "__SchemaSubstitutionPlugin__recur__mark__";
+
 	public boolean validate(List<String> paramList) {
 		return true;
 	}
 
 	public void initialized(IntrospectedTable introspectedTable) {
 		final FullyQualifiedTable fqt = introspectedTable.getFullyQualifiedTable();
-		
-		//use proxy method to intercept and change runtime table name
-		introspectedTable.setFullyQualifiedTable((FullyQualifiedTable)Proxy.newProxyInstance(
-						fqt.getClass().getClassLoader(), 
-						new Class[]{FullyQualifiedTable.class}, 
-						new InvocationHandler(){
-							public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-								String methodName = method.getName();
-								if("getFullyQualifiedTableNameAtRuntime".equals(methodName)){
-									Object fullyQualifiedTableNameAtRuntime = method.invoke(fqt, args);
-									if(fullyQualifiedTableNameAtRuntime!=null){
-										String modified = fullyQualifiedTableNameAtRuntime.toString();
-										int dot=modified.lastIndexOf(".");
-										if(dot==-1) {
-											return "${schema}."+modified;
-										}
-										else {
-											return modified.substring(0, dot) +"${schema}"+ modified.substring(dot);
-										}
-									}
-									return fullyQualifiedTableNameAtRuntime;
-								}
-								
-								return method.invoke(fqt, args);
-							}
-						}//end of InvocationHandler inner class
-					));//proxy end，end setFullyQualifiedTable
-		
-		//this is one duplicated invocation, the purpose is to trigger calling calculateXmlAttributes, final to ullyQualifiedTable.getFullyQualifiedTableNameAtRuntime()
-		introspectedTable.initialize();
+		try {
+			Field isf=fqt.getClass().getDeclaredField("runtimeTableName");
+			isf.setAccessible(true);
+			Object value=isf.get(fqt);
+			if(value!=null){
+				String strValue = value.toString();
+				isf.set(fqt, "${schema}."+strValue);
+			}
+			
+			Field rsf=fqt.getClass().getDeclaredField("introspectedTableName");
+			rsf.setAccessible(true);
+			value=rsf.get(fqt);
+			if(value!=null){
+				String strValue = value.toString();
+				rsf.set(fqt, "${schema}."+strValue);
+			}
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		//this is one duplicated invocation, the purpose is to trigger calling calculateXmlAttributes, 
+		//final to ullyQualifiedTable.getFullyQualifiedTableNameAtRuntime()
+		Object mark=introspectedTable.getAttribute(RECUR_MARK);
+		if(mark==null){
+			introspectedTable.setAttribute(RECUR_MARK, RECUR_MARK);
+			introspectedTable.initialize();
+		}
 	}
 }
