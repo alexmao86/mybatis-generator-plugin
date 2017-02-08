914135ec3565722686a14f91d579389ba99e6cd6
alexmao86 <maoanapex88@163.com>
2017-02-08 20:47:07 +0800

    remove unused import

--- src/main/java/net/sourceforge/jweb/mybatis/generator/plugins/SchemaSubstitutionPlugin.java
+++ src/main/java/net/sourceforge/jweb/mybatis/generator/plugins/SchemaSubstitutionPlugin.java
@@ -12,7 +12,6 @@ import org.mybatis.generator.api.dom.java.JavaVisibility;
 import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.Parameter;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
-import org.mybatis.generator.api.dom.xml.XmlElement;
 import org.mybatis.generator.logging.Log;
 import org.mybatis.generator.logging.LogFactory;
 
